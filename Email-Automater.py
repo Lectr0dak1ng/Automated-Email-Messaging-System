93756a2b7a076405c6ff373b17ba1ded87501a03
Abinesh <145048407+Lectr0dak1ng@users.noreply.github.com>
2025-05-04 00:40:38 +0530

    Email-Automater.py

---
+++ Email-Automater.py
@@ -0,0 +1,46 @@
+from email.mime.text import MIMEText 
+from email.mime.image import MIMEImage 
+from email.mime.application import MIMEApplication 
+from email.mime.multipart import MIMEMultipart 
+import smtplib #Simple Mail Transfer Protocol
+import os 
+smtp = smtplib.SMTP('smtp.gmail.com', 587) 
+smtp.ehlo() 
+smtp.starttls()  
+smtp.login('your email id', 'authenticated passowrd') 
+def message(subject="Python Notification", 
+			text="", img=None, 
+			attachment=None): 
+	msg = MIMEMultipart() 
+	msg['Subject'] = subject 
+	msg.attach(MIMEText(text)) 
+	if img is not None: 
+		if type(img) is not list: 
+			img = [img] 
+		for one_img in img: 
+			img_data = open(one_img, 'rb').read()  
+			msg.attach(MIMEImage(img_data, 
+								name=os.path.basename(one_img))) 
+	if attachment is not None: 
+		if type(attachment) is not list: 
+			attachment = [attachment] 
+
+		for one_attachment in attachment: 
+
+			with open(one_attachment, 'rb') as f: 
+				file = MIMEApplication( 
+					f.read(), 
+					name=os.path.basename(one_attachment) 
+				) 
+				file['Content-Disposition'] = f'attachment; filename="{os.path.basename(one_attachment)}"'
+			msg.attach(file) 
+	return msg 
+
+msg = message("Good!", "Hi there!") 
+
+to = ["reciever email id", 
+	"reciever email i"] 
+
+smtp.sendmail(from_addr="your email id", 
+			to_addrs=to, msg=msg.as_string()) 
+smtp.quit()
